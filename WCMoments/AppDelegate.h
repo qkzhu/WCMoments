923381b6afa5ef0740977a2383f61bc76f0cbd2e
QianKun <zqkun.public@gmail.com>
2016-09-09 22:21:48 +0800

    Initial Commit

---
+++ WCMoments/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WCMoments
+//
+//  Created by QianKun on 9/9/16.
+//  Copyright © 2016 QianKun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WCMoments/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WCMoments
+//
+//  Created by QianKun on 9/9/16.
+//  Copyright © 2016 QianKun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
